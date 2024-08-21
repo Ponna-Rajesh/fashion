9e4f699fb8fbd0f34bbfa75a0362ce8ce90058c8
Rajesh <71017498+Ponna-Rajesh@users.noreply.github.com>
2024-08-21 20:16:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import Body from "./components/Body"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+// import Body from "./components/Body"
+// import Header from "./components/Header"
+// import Footer from "./components/Footer"
 
 
 
-function App() {
-  return (
-    <>
-    <div className="w-full max-w-[95rem] mx-auto">
-        <Header />
-        <Body />
-        <Footer />
-    </div>
+// function App() {
+//   return (
+//     <>
+//     <div className="w-full max-w-[95rem] mx-auto">
+//         <Header />
+//         <Body />
+//         <Footer />
+//     </div>
   
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default App
+// export default App
